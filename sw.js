4bb4e3b6bd61d603714808d8bf724354827a3f28
Abu Sazid <amiete66@gmail.com>
2026-02-23 00:30:08 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,83 @@
+const CACHE_NAME = 'ramadan-cache-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/azan.mp3',
+  '/icon.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          response => {
+            // Check if valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Background sync for notifications
+self.addEventListener('sync', event => {
+  if (event.tag === 'ramadan-sync') {
+    event.waitUntil(checkRamadanTimes());
+  }
+});
+
+function checkRamadanTimes() {
+  // This will be used for background notifications
+  return self.registration.showNotification('ৰমজান সময়সূচী', {
+    body: 'আজিৰ সময়সূচী চাওক',
+    icon: 'icon.png',
+    badge: 'icon.png'
+  });
+}
